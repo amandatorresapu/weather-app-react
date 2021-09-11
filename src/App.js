701c1dda4267ec4c869636045078eb03457180b8
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-09-11 16:15:38 -0700

    added git hub at the bottom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="SearchEngine">
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+        <a href="https://github.com/amandatorresapu/weather-app-react">
+          Open source
+        </a>{" "}
+        by Amanda Torres-Brooks
+      </p>
     </div>
   );
 }
